5d07afac57bac38a3a7799222f7699c043b9e9ac
lock <srleo@qq.com>
2019-02-13 16:25:10 +0800

    2018/02/13

---
+++ src/editor/components/requests.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <el-table :data="list" :show-header="false">
+            <el-table-column v-if="editable"
+                             type="index"
+                             width="50">
+            </el-table-column>
+            <el-table-column>
+                <template slot-scope="scope">
+                    <el-input v-model="list[scope.$index]" @change="$emit('changed')"
+                              @keypress.enter.native="checkEmpty"
+                              @blur="checkEmpty"></el-input>
+                </template>
+            </el-table-column>
+            <el-table-column v-if="editable" width="60px">
+                <template slot-scope="scope">
+                    <el-button icon="el-icon-delete" type="text" circle
+                               @click="deleteRequest(scope.$index, scope.row)"></el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+  import {ConfirmDeleteRequest, GetLanguageString, PromptHowToGetTheRequestUrl} from "../../js/i18_string_name";
+
+  let lastAddRequestInput;
+  export default {
+    props: ['list', 'editable'],
+    data() {
+      return {
+        ui: {
+          delete: GetLanguageString(ConfirmDeleteRequest),
+          add: GetLanguageString(PromptHowToGetTheRequestUrl),
+        },
+        addNew: {add: true, value: '', index: '+'},
+      };
+    },
+    methods: {
+      addRequest: async function () {
+        let {value} = await this.$prompt(this.ui.add, {
+          inputValidator: val => {
+            return !!val && val.trim().length > 0;
+          },
+        });
+        let find = this.list.indexOf(value);
+        if (find !== -1) {
+          this.$el.querySelectorAll('input')[find].focus();
+          return;
+        }
+        this.list.splice(0, 0, value);
+        this.$nextTick(() => {
+          this.$el.querySelector('input').focus();
+        });
+        this.$emit('changed');
+      },
+      async deleteRequest(index, value) {
+        if (this.list.length > index) {
+          await this.$confirm(this.ui.delete.replace('%value', value));
+          this.list.splice(index, 1);
+          this.$emit('changed');
+        }
+      },
+      checkEmpty() {
+        let a = 0;
+        for (let i = 0; i < this.list.length; i++) {
+          const value = this.list[i], indexOf = this.list.lastIndexOf(value);
+          if (value.length === 0 || (indexOf !== -1 && indexOf !== i)) {
+            a++;
+            this.list.splice(i, 1);
+          }
+        }
+        if (a > 0)
+          this.$emit('changed');
+      },
+    },
+    mounted() {
+      lastAddRequestInput = this.$el.querySelector('input');
+      console.log({lastAddRequestInput});
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
